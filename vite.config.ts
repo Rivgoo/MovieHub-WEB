9e4fbba5352278d8766c44e02342057d3c625cad
Pavlo Panko <pavlikosar@gmail.com>
2025-04-26 20:07:17 +0300

    feat: move api config to .env

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
   server: {
     port: 5173,
     strictPort: true,
-  }
-})
+  },
+});
+
+interface ImportMetaEnv {
+  readonly VITE_API_BASE_URL: string;
+}
