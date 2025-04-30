2aedbffcc8c6d17006d7698b1daaa9b1640fad78
Pavlo Panko <pavlikosar@gmail.com>
2025-04-30 20:27:45 +0300

    feat: add admin manager pages

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,9 @@ import { Suspense } from 'react';
 import React from 'react';
 import { UserRole } from './core/auth/types';
 
-
+{
+  /* Pages */
+}
 const LoginPage = React.lazy(() => import('./features/Login/LoginPage'));
 const HomePage = React.lazy(() => import('./features/Home/HomePage'));
 const AboutPage = React.lazy(() => import('./features/About/AboutPage'));
@@ -27,15 +29,37 @@ const SessionSearchPage = React.lazy(
 const CustomerAccountPage = React.lazy(
   () => import('./features/Customer/AccountPage')
 );
-const AdminDashboardPage = React.lazy(
-  () => import('./features/Admin/DashboardPage/DashboardPage')
-);
 const FavoritePage = React.lazy(
   () => import('./features/Customer/FavoritePage')
 );
 const BookingPage = React.lazy(() => import('./features/Customer/BookingPage'));
 const RegistrationPage = React.lazy(
   () => import('./features/Registration/RegistrationPage')
+);
+
+{
+  /* Admin Pages */
+}
+const FilmManagerPage = React.lazy(
+  () => import('./features/Admin/Film/FilmManagerPage')
+);
+const SessionManagerPage = React.lazy(
+  () => import('./features/Admin/Session/SessionManagerPage')
+);
+const CinemaHallManagerPage = React.lazy(
+  () => import('./features/Admin/CinemaHall/CinemaHallManagerPage')
+);
+const GenreManagerPage = React.lazy(
+  () => import('./features/Admin/Genre/GenreManagerPage')
+);
+const ActorManagerPage = React.lazy(
+  () => import('./features/Admin/Actor/ActorManagerPage')
+);
+const UserManagerPage = React.lazy(
+  () => import('./features/Admin/User/UserManagerPage')
+);
+const BookingManagerPage = React.lazy(
+  () => import('./features/Admin/Booking/BookingManagerPage')
 );
 
 const LoadingFallback = () => (
@@ -51,13 +75,13 @@ const LoadingFallback = () => (
 );
 
 function App() {
-
   return (
     <BrowserRouter>
       <AuthProvider>
         <ThemeProvider theme={theme}>
           <Suspense fallback={<LoadingFallback />}>
             <Routes>
+              {/* Public Routes */}
               <Route path="/login" element={<LoginPage />} />
               <Route path="/registration" element={<RegistrationPage />} />
               <Route path="/about" element={<AboutPage />} />
@@ -69,6 +93,7 @@ function App() {
               <Route path="/session/:id" element={<SessionPage />} />
               <Route path="/session-search" element={<SessionSearchPage />} />
 
+              {/* User Routes */}
               <Route
                 path="/account"
                 element={
@@ -77,6 +102,7 @@ function App() {
                   </ProtectedRoute>
                 }
               />
+
               <Route
                 path="/account/favorite"
                 element={
@@ -93,12 +119,70 @@ function App() {
                   </ProtectedRoute>
                 }
               />
+
+              {/* Admin Routes */}
               <Route
                 path="/admin/dashboard"
                 element={
-                //   <ProtectedRoute allowedRoles={[UserRole.Admin]}>
-                    <AdminDashboardPage />
-                //   </ProtectedRoute>
+                  <ProtectedRoute allowedRoles={[UserRole.Admin]}>
+                    <FilmManagerPage />
+                  </ProtectedRoute>
+                }
+              />
+              <Route
+                path="/admin/film-manager"
+                element={
+                  <ProtectedRoute allowedRoles={[UserRole.Admin]}>
+                    <FilmManagerPage />
+                  </ProtectedRoute>
+                }
+              />
+              <Route
+                path="/admin/session-manager"
+                element={
+                  <ProtectedRoute allowedRoles={[UserRole.Admin]}>
+                    <SessionManagerPage />
+                  </ProtectedRoute>
+                }
+              />
+              <Route
+                path="/admin/cinema-hall-manager"
+                element={
+                  <ProtectedRoute allowedRoles={[UserRole.Admin]}>
+                    <CinemaHallManagerPage />
+                  </ProtectedRoute>
+                }
+              />
+              <Route
+                path="/admin/genre-manager"
+                element={
+                  <ProtectedRoute allowedRoles={[UserRole.Admin]}>
+                    <GenreManagerPage />
+                  </ProtectedRoute>
+                }
+              />
+              <Route
+                path="/admin/actor-manager"
+                element={
+                  <ProtectedRoute allowedRoles={[UserRole.Admin]}>
+                    <ActorManagerPage />
+                  </ProtectedRoute>
+                }
+              />
+              <Route
+                path="/admin/user-manager"
+                element={
+                  <ProtectedRoute allowedRoles={[UserRole.Admin]}>
+                    <UserManagerPage />
+                  </ProtectedRoute>
+                }
+              />
+              <Route
+                path="/admin/booking-manager"
+                element={
+                  <ProtectedRoute allowedRoles={[UserRole.Admin]}>
+                    <BookingManagerPage />
+                  </ProtectedRoute>
                 }
               />
 
