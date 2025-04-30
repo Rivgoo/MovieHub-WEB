86073a44433fd8bd663aa8c70b57fde1e3e5cd5a
Pavlo Panko <pavlikosar@gmail.com>
2025-04-30 20:53:47 +0300

    feat: add load user info from api

--- src/features/Admin/Topbar/Topbar.tsx
+++ src/features/Admin/Topbar/Topbar.tsx
@@ -1,23 +1,68 @@
 import { Box, Typography } from '@mui/material';
 import './Topbar.css';
 import { useState, useEffect } from 'react';
+import { getMyUserInfo, UserInfoResponse } from '../../../core/api/userApi';
 
 const Topbar = () => {
-  const [menuOpen] = useState(false);
+  const [userInfo, setUserInfo] = useState<UserInfoResponse | null>(null);
+  const [loading, setLoading] = useState<boolean>(true);
+  const [error, setError] = useState<string | null>(null);
 
   useEffect(() => {
-    return () => {
-      document.body.style.overflow = '';
+    const fetchUserInfo = async () => {
+      setLoading(true);
+      setError(null);
+
+      try {
+        const data = await getMyUserInfo();
+        setUserInfo(data);
+      } catch (err) {
+        setError('Failed to load user info.');
+      } finally {
+        setLoading(false);
+      }
     };
-  }, [menuOpen]);
+
+    fetchUserInfo();
+  }, []);
+
+  const renderProfileContent = () => {
+    if (loading) {
+      return (
+        <>
+          <Typography className="profileName">&nbsp;</Typography>
+          <Typography className="profileRole">&nbsp;</Typography>
+        </>
+      );
+    }
+    if (error) {
+      return (
+        <Typography className="profileName" color="error">
+          {error}
+        </Typography>
+      );
+    }
+    if (userInfo) {
+      return (
+        <>
+          <Typography className="profileName">
+            {`${userInfo.firstName || ''} ${userInfo.lastName || ''}`.trim() ||
+              'User'}
+          </Typography>
+          <Typography className="profileRole">{userInfo.email}</Typography>
+        </>
+      );
+    }
+
+    return (
+      <Typography className="profileName">User Info Unavailable</Typography>
+    );
+  };
 
   return (
     <Box className="topbar">
       <Box className="profileBox">
-        <Box>
-          <Typography className="profileName">UserName</Typography>
-          <Typography className="profileRole">Email</Typography>
-        </Box>
+        <Box sx={{ textAlign: 'right' }}>{renderProfileContent()}</Box>
       </Box>
     </Box>
   );
